5be2cfd83b54967cba93c47ba8c5e1e7f2a09565
jmin4078 <wpalsrl8588@gmail.com>
2026-07-31 09:17:40 +0900

    Initial commit

---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>걱정인형</title>
+</head>
+<body>
+<h1>걱정인형</h1>
+</body>
+</html>
---
+++ src/test/java/org/example/worrydoll/WorrydollApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.worrydoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorrydollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
