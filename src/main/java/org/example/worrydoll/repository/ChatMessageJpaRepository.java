c467ec6d55f357a7dd3a4f761ba0f121c3fcc467
jmin4078 <wpalsrl8588@gmail.com>
2026-07-31 11:20:49 +0900

    Add chat message persistence

---
+++ src/main/java/org/example/worrydoll/repository/ChatMessageJpaRepository.java
@@ -0,0 +1,18 @@
+package org.example.worrydoll.repository;
+
+import org.example.worrydoll.entity.ChatMessage;
+import org.example.worrydoll.entity.ChatUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatMessageJpaRepository extends JpaRepository<ChatMessage, Long> {
+
+    @Query("SELECT DISTINCT c.conversationId FROM ChatMessage c")
+    List<String> findConversationIds();
+
+    List<ChatMessage> findAllByConversationId(String conversationId);
+    void deleteAllByConversationId(String conversationId);
+}
